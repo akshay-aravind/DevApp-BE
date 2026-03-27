e0d35bd927792cd1b7f8f96673c5ee97e8d5b672
Akshay Aravind <akshayaravind@netstratum.com>
2026-03-27 20:33:35 +0530

    test: log added

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  console.log('Trying to Connect')
   await mongoose.connect('mongodb+srv://sky:%23123password@devtinder.t4f7mw8.mongodb.net/devTinder');
 };
 
